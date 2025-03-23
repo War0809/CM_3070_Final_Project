c34e0cd3c0f642b945fea95bd1ffc56eaeba01fe
War0809 <96650489+War0809@users.noreply.github.com>
2025-03-23 16:53:31 +0100

    Update constant.dart

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-
-/// [client] for Supabase instance
 final client = Supabase.instance.client;
 
-/// Small Gap
 const smallGap = SizedBox(
   height: 15,
 );
 
-/// Large Gap
 const largeGap = SizedBox(
   height: 30,
 );
 
-///Custom SnackBar
 extension ShowSnackBar on BuildContext{
   void showErrorMessage(String message){
     ScaffoldMessenger.of(this).showSnackBar(
